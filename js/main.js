62139e3a2987df991a83638b4afb166c0accf9c0
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-03-25 14:40:52 +0300

    Add mega menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// Init ParticlesJS
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded')
 })
+
+// Show dropdown menu on hover
+$(document).ready(function(){
+    $(".dropdown").hover(function() {
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        var navLink = $(this).children(".nav-link");
+
+        dropdownMenu.toggleClass("show");
+        dropdownMenu.parent().toggleClass("show");
+        navLink.toggleClass("active");
+    });
+});
