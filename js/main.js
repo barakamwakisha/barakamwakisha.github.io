e23122f71433e4166f42743be76091e944550ec1
Baraka Mwakisha <barakamwakisha@gmail.com>
2020-03-25 16:39:01 +0300

    Add aux page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 // Init ParticlesJS
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function () {
     console.log('callback - particles.js config loaded')
 })
 
 // Show dropdown menu on hover
-$(document).ready(function(){
-    $(".dropdown").hover(function() {
+$(document).ready(function () {
+    $(".dropdown").hover(function () {
         var dropdownMenu = $(this).children(".dropdown-menu");
         var navLink = $(this).children(".nav-link");
 
         dropdownMenu.toggleClass("show");
         dropdownMenu.parent().toggleClass("show");
         navLink.toggleClass("active");
+    });
+});
+
+$(document).ready(function () {
+    $(".ct-js-btn-scroll").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
     });
 });
 
